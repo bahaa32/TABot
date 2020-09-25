64366f10538a12e5444362397eeb2fc212f89a10
Bahaa <beldin222@gmail.com>
2020-09-25 08:45:32 -0700

    Move commands logic to commands cog

--- cogs/control.py
+++ cogs/control.py
@@ -5,13 +5,12 @@ from discord.ext import commands
 class Control_Panel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = self.bot.get_cog("Config")
-        self.queue = self.bot.get_cog("Queue")
-        self.embed_generator = self.bot.get_cog("Embeds")
+        self.config = bot.get_cog("Config")
+        self.queue = bot.get_cog("Queue")
+        self.embed_generator = bot.get_cog("Embeds")
 
     # TODO: Use cog checks to check for role instead (If that's possible?)
-    @commands.command()
-    async def next(self, ctx):
+    async def move_next_user(self, ctx):
         ta_role_id = int(self.config.get_server_config(
             ctx.author.guild.id, "ta_role_id"))
         ta_role = ctx.author.guild.get_role(ta_role_id)
@@ -26,21 +25,6 @@ class Control_Panel(commands.Cog):
             else:
                 await ctx.send("No more users in queue.")
 
-    @commands.command(name="queue")
-    async def queue_query(self, ctx):
-        ta_role_id = int(self.config.get_server_config(
-            ctx.author.guild.id, "ta_role_id"))
-        is_ta = True if discord.utils.find(
-            lambda role: role.id == ta_role_id, ctx.author.roles) else False
-        if is_ta:
-            if not ctx.author.guild.id in self.queue.queue:
-                return await ctx.send("Nobody is in queue.")
-            if len(self.queue.queue[ctx.author.guild.id]) == 0:
-                return await ctx.send("Nobody is in queue.")
-            waiting_list = ""
-            for idx, member in enumerate(self.queue.queue[ctx.author.guild.id], start=1):
-                waiting_list += f"{idx}. {member.name}\n"
-            await ctx.send(f"Users in queue right now:\n```{waiting_list}```")
 
 def setup(bot):
     bot.add_cog(Control_Panel(bot))
